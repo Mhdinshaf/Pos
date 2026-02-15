f0d1114edffd9f5ddd377a2692a25c644228c4ef
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-15 23:43:54 +0530

    Feat: Enhance SignUp form with fx:id attributes and createAccount action method

--- src/main/java/org/Pos/controller/login/SignUpController.java
+++ src/main/java/org/Pos/controller/login/SignUpController.java
@@ -1,4 +1,31 @@
 package org.Pos.controller.login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
+
+    @FXML
+    private ComboBox<?> selectRole;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtIdNumber;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void createAccountOnAction(ActionEvent event) {
+
+    }
+
 }
