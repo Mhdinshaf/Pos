3bce7184d11b7c81da425b6ea5912fa947cab326
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-04-01 00:13:43 +0530

    feat: add User model class

---
+++ src/main/java/com/pos/model/User.java
@@ -0,0 +1,23 @@
+package com.pos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "password")
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
