2666ffb24c099a5cd9a2b1fabc3521b7ff34e047
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-04-01 01:47:57 +0530

    feat: recreate Main and starter entry point in com.pos package

---
+++ src/main/java/com/pos/starter.java
@@ -0,0 +1,24 @@
+package com.pos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class starter extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Clothify Store");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
